6cd20b966a9d49d4300e87ad8d22aa7fa7ed2d9a
Ranch <vadiqbozhko@gmail.com>
2026-02-09 17:16:30 +0800

    shix

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./workout_app.db"
+# Используем переменную окружения DATABASE_URL, если она есть
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./workout_app.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
